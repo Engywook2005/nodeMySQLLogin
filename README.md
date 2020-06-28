fed2e42492cafd735e1e24ec3899c2d258f27439
Engywook2005 <uig6fl*ht1ft>
2020-06-27 21:57:26 -0400

    Added login form

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 All files to be served come from here
 
 #### app
-Single-page app and js
+Single-page app and js. 
 
 #### public
 Non-sensitive static html plus assets: css, js, etc.
 
+#### queries
+Query handling urls. Target of AJAX calls, response is JSON.
+
 #### registration
-Login and registration forms
+Login and registration forms.
--- server/routing/appRouting.js
+++ server/routing/appRouting.js
@@ -3,12 +3,19 @@ class AppRouting {
         this.app = app;
     }
     init() {
+
+        // @TODO make this a constant
         const htmlPath = `${process.cwd()}/html/`;
 
         this.app.get('/', (request, response) => {
-            // @TODO check request.session.
+            if(request.session.token) {
 
-            response.sendFile(`${htmlPath}/app/spa.html`);
+                // @TODO need to get token to spa.html... see this: https://stackoverflow.com/questions/16369649/include-html-blocks-using-node-js and https://ejs.co/
+                // Actually best, most readable resource: https://www.geeksforgeeks.org/use-ejs-as-template-engine-in-node-js. Need to use ejs
+                response.sendFile(`${htmlPath}/app/spa.html`);
+            } else {
+                response.sendFile(`${htmlPath}/registration/login.html`);
+            }
         });
     }
 }
